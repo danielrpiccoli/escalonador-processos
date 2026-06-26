64d1af61c15056fba8d2b596993514880be1123b
daniel <danielrpiccoli@gmail.com>
2026-06-26 11:04:52 -0300

    fila.h criado e implementado

---
+++ fila.h
@@ -0,0 +1,19 @@
+#ifndef FILA_H
+#define FILA_H
+
+#include "pcb.h"
+#define MAX_FILA 100
+
+typedef struct {
+    PCB* itens[MAX_FILA];
+    int inicio;
+    int fim;
+    int tamanho;
+} Fila;
+
+void fila_init(Fila* f);
+int fila_vazia(Fila* f);
+void enfileirar(Fila* f, PCB* p);
+PCB* desenfileirar(Fila* f);
+
+#endif
